6a11869831901231957e7dc922eb89c1f0df79bc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-17 15:48:14 -0400

    Fixes print statements in graph.py to make print outs more readable

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -4,21 +4,25 @@ Simple graph implementation
 from util import Stack, Queue  # These may come in handy
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """Represents a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
     def add_vertex(self, vertex):
         """
-        Add a vertex to the graph.
+        Adds a vertex to the graph.
         """
-        self.vertices[vertex] = set()  #  (1) Create an empty set
+        self.vertices[vertex] = set()  #  (1) Creates a node or vertex in the set
     def add_edge(self, v1, v2):
         """
-        Add a directed edge to the graph.
+        Adds a directed edge to the graph.
         """
-        if v1 in self.vertices and v2 in self.vertices:
+        #  If both vertices exist in the graph (the set)...
+        if v1 in self.vertices and v2 in self.vertices:  
+            #  Add an edge from v1 to v2
             self.vertices[v1].add(v2)
+        #  If both vertices DO NOT exist in the graph (the set)...
         else:
+            #  Thow an error
             raise IndexError("That vertex does not exist!")
     def bft(self, starting_vertex):
         """
@@ -87,29 +91,23 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-
-        visited = set()
+        
         #  (1) Create an empty set to store visited nodes
         #  (2) Create an empty Queue and enqueue A PATH TO the starting vertex
-        q = Queue()
-        q.enqueue(starting_vertex)
         #  (3) While the queue is not empty...
-        while q.size() > 0:
             #  (a) Dequeue the first PATH
             #  (b) GRAB THE VERTEX FROM THE END OF THE PATH
-            v = q.dequeue()
             #  (c) If that vertex has not been visited...
-            if v not in visited:
                 #  (i) Mark it as visited
-                visited.add(v)
-                print(v)
                 #  (ii) Then, add A PATH TO all of its neighbors to the 
                 #       back of the queue
                 #  (iii) Copy the path
                 #  (iv) Append the neighbor to the back of the copy
                 #  (v) Enqueue the copy
-                for neighbor in self.vertices[v]:
-                    q.enqueue(neighbor)
+                    
+                    
+                    
+                    
                     
     # q = [ [1,2,4], [1,2,3,5]]
     # visited = {1, 2}
@@ -139,6 +137,7 @@ if __name__ == '__main__':
     graph.add_vertex(5)
     graph.add_vertex(6)
     graph.add_vertex(7)
+    graph.add_vertex(8)  #  <== checks to make sure error handling works properly
     graph.add_edge(5, 3)
     graph.add_edge(6, 3)
     graph.add_edge(7, 1)
@@ -154,7 +153,10 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
+    print("---------------------------------------------")
+    print("Vertices of the Graph:\n") 
     print(graph.vertices)
+    print("\n---------------------------------------------")
 
     '''
     Valid DFT paths:
@@ -163,7 +165,9 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
+    print("Depth First TRAVERSAL path for (1, 6):\n",)
     graph.dft(1)
+    print("\n", "---------------------------------------------")
 
     '''
     Valid BFT paths:
@@ -180,7 +184,9 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
+    print("Breadth First TRAVERSAL path for (1, 6):\n",)
     graph.bft(1)
+    print("\n", "---------------------------------------------")
 
     '''
     Valid DFT recursive paths:
@@ -189,17 +195,23 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
+    print("Depth First TRAVERSAL (recursive) path for (1, 6):\n")
     graph.dft_recursive(1)
+    print("\n", "---------------------------------------------")
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    print("Breadth First SEARCH path for (1, 6):\n")
+    graph.bfs(1, 6)
+    print("\n", "---------------------------------------------")
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
+    print("Depth First SEARCH path for (1, 6):\n")
+    graph.dfs(1, 6)
+    print("\n", "---------------------------------------------")
