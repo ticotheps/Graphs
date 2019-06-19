66d6b499b083ac09d1351e56f0e537d68c2180b2
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-19 00:14:19 -0400

    This solution does not pass the provided test, but does pass all of the tests I have written, that are based on the README example and the constraints in the problem

--- projects/ancestor/test_ancestor.py
+++ projects/ancestor/test_ancestor.py
@@ -1,17 +1,18 @@
 import unittest
-from ancestor import earliest_ancestor
+from ancestor import Graph
+
+earliest_ancestor = Graph.earliest_ancestor
 
 class Test(unittest.TestCase):
 
-    '''
-       10
-     /
-    1   2   4  11
-     \ /   / \ /
-      3   5   8
-       \ / \   \
-        6   7   9
-    '''
+#    10
+#  /
+# 1   2   4  11
+#  \ /   / \ /
+#   3   5   8
+#    \ / \   
+#     6   7   9
+
     def test_earliest_ancestor(self):
         test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
         self.assertEqual(earliest_ancestor(test_ancestors, 1), 10)
